7ba11480bfed14bb2a77d5c6b9e0d4fdd3f2512b
jlnextu <jlnextu@gmail.com>
2017-11-07 14:15:10 -0500

    new commit

---
+++ src/holamundo/HolaMundo.java
@@ -0,0 +1,13 @@
+/*
+ * jl_
+*/
+package holamundo;
+
+import javax.swing.JOptionPane;
+
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hola Mundo Java ! ");
+    }
+}
